ba7679d2cd9ba5a8ada2da244da0a5150f1a5202
Nick Stokoe <github.wu-lee@noodlefactory.co.uk>
2015-07-09 22:10:35 +0100

    nim.cfg / blink.nim - working combination for makefile

--- blink.nim
+++ blink.nim
@@ -6,24 +6,24 @@ const
 type
   byteaddr = ptr uint8
 
-proc blank(x:int) :int =
-  var
-    delay : uint16
-    value : uint8 = 0
-    DDRB {.volatile.} : byteaddr = cast[byteaddr](DDRBaddr)
-    PORTB {.volatile.} : byteaddr = cast[byteaddr](PORTBaddr)
+#proc blank(x:int) :int =
+var
+  delay : uint16
+  value : uint8 = 0
+  DDRB {.volatile.} : byteaddr = cast[byteaddr](DDRBaddr)
+  PORTB {.volatile.} : byteaddr = cast[byteaddr](PORTBaddr)
 
 
-  # Set pin 5 of port b for output
-  DDRB[] = 255
+# Set pin 5 of port b for output
+DDRB[] = 255
 
-  while true:
-    value = not value
-    delay = 32000
-    while delay != 0:
-      delay -= 1
-    PORTB[] = value
-  return 0
+while true:
+  value = not value
+  delay = 32000
+  while delay != 0:
+    delay -= 1
+  PORTB[] = value
+#  return 0
 
-discard blank(3)
+#discard blank(3)
 
