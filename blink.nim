0af15a18681816894510d793b842cf19252f026d
Nick Stokoe <github.wu-lee@noodlefactory.co.uk>
2015-07-15 23:10:00 +0100

    blink.nim - use _delay_ms() - works!

--- blink.nim
+++ blink.nim
@@ -2,28 +2,30 @@ import unsigned
 const
   DDRBaddr = 0x04 + 0x20
   PORTBaddr = 0x05 + 0x20
+  arduinoPath = "/usr/share/arduino/hardware/arduino/cores/arduino/"
+
+{.passC: "-DF_CPU=16000000L".}
+{.passC: "-I" & arduinoPath.}
+{.passC: "-I" & arduinoPath & "../../variants/standard/".}
+
+{.compile: arduinoPath & "wiring.c".}
 
 type
   byteaddr = ptr uint8
 
-#proc blank(x:int) :int =
 var
-  delay : uint16
-  value : uint8 = 0
+  value : uint8 = 255
   DDRB {.volatile.} : byteaddr = cast[byteaddr](DDRBaddr)
   PORTB {.volatile.} : byteaddr = cast[byteaddr](PORTBaddr)
 
+proc delay_ms(ms: uint16) {.importc: "_delay_ms", header: "util/delay.h" .}
 
 # Set pin 5 of port b for output
 DDRB[] = 255
 
+PORTB[] = value
 while true:
   value = not value
-  delay = 32000
-  while delay != 0:
-    delay -= 1
   PORTB[] = value
-#  return 0
-
-#discard blank(3)
+  delay_ms(100)
 
